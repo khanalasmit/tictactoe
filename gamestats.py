e947635258533a978910b56aa0432f53e884245a
khanalasmit <asmitpasa2063@gmail.com>
2024-12-30 12:51:17 +0545

    Update gamestats.py

--- gamestats.py
+++ gamestats.py
@@ -1,34 +1,30 @@
 class Stats:
-    def __init__(self,screen):
-        self.signstat=1
+    def __init__(self, screen):
+        self.signstat = 1
         self.winner_combination = [
-        [0, 1, 2], [3, 4, 5], [6, 7, 8],  # Horizontal
-        [0, 3, 6], [1, 4, 7], [2, 5, 8],  # Vertical
-        [0, 4, 8], [2, 4, 6]              # Diagonal
+            [0, 1, 2], [3, 4, 5], [6, 7, 8],
+            [0, 3, 6], [1, 4, 7], [2, 5, 8],
+            [0, 4, 8], [2, 4, 6]
         ]
-        self.sideA=[]
-        self.sideB=[]
-        self.board=["-", "-", "-", "-", "-", "-", "-", "-", "-"]
-        self.game_active=False
-        
+        self.sideA = []
+        self.sideB = []
+        self.board = ["-", "-", "-", "-", "-", "-", "-", "-", "-"]
+        self.game_active = False
+
     def winner(self):
         for combination in self.winner_combination:
-            if all(pos in self.sideA for pos in combination):  # Check if `sideA` has a winning combination
+            if all(pos in self.sideA for pos in combination):
                 return 1
-            if all(pos in self.sideB for pos in combination):  # Check if `sideB` has a winning combination
+            elif all(pos in self.sideB for pos in combination):
                 return -1
-    def gameover(self):
-    # Check for all winning conditions (rows, columns, diagonals)
-        if (self.board[0] == self.board[1] == self.board[2] != "-") or \
-        (self.board[3] == self.board[4] == self.board[5] != "-") or \
-        (self.board[6] == self.board[7] == self.board[8] != "-") or \
-        (self.board[0] == self.board[3] == self.board[6] != "-") or \
-        (self.board[1] == self.board[4] == self.board[7] != "-") or \
-        (self.board[2] == self.board[5] == self.board[8] != "-") or \
-        (self.board[0] == self.board[4] == self.board[8] != "-") or \
-        (self.board[2] == self.board[4] == self.board[6] != "-"):
-            return True
-        else:
-            return False
+        if "-" not in self.board:
+            return 0
+        return None
 
-        
+    def gameover(self):
+        for combination in self.winner_combination:
+            if self.board[combination[0]] == self.board[combination[1]] == self.board[combination[2]] != "-":
+                return True
+        if "-" not in self.board:
+            return True
+        return False
